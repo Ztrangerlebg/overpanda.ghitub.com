6f5326bde7c81c1e8dcfa9a4836201433017e5de
Ztrangerlebg <90959480+Ztrangerlebg@users.noreply.github.com>
2021-10-18 17:46:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+ jQuery( document ).ready(function() { 
+  jQuery('.love').click(function(){
+    jQuery(this).html('<p class="final">Joyeux anniversaire Helios &nbsp;!</p><img src="https://media.giphy.com/media/feio2yIUMtdqWjRiaF/giphy.gif" width="480" height="270" alt="bon anniv !">');
+  });
